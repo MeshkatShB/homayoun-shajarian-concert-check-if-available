d935d8ced3b01938bce7ce068480debf705abf49
MeshkatShB <meshkat97@yahoo.com>
2022-06-13 15:24:12 +0430

    Bug fix

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 import sched
 import time
 import os
+from bs4 import BeautifulSoup
 
 import requests as req
 
 is_reserve_time_open = False
 URL = 'https://www.tiwall.com/s/homayoun.shajarian'
+TIWALL_URL = 'https://www.tiwall.com/'
 INTERVALS = 60
 PRIORITY = 1
-
-
-def main(response):
-    """
-    Set Scheduler to Dispatch check_if_reserve_open function at  Intervals
-    :param response: Response of request to the URL
-    :return: None
-    """
-    s = sched.scheduler(time.time, time.sleep)
-    s.enter(INTERVALS, PRIORITY, check_if_reserve_open, (s, response, ))
-    s.run()
 
 
 def check_if_reserve_open(sc, response):
@@ -41,7 +32,18 @@ def check_if_reserve_open(sc, response):
     #     print("JESUS, MOVE IT! Go Reserve Bro!", response.history[0].url)
     #     os.system('spd-say "You Can Get Your Concert Ticket Now"')
 
-    sc.enter(60, 1, check_if_reserve_open, (sc,))
+    sc.enter(60, 1, check_if_reserve_open, (sc, response))
+
+
+def main(response):
+    """
+    Set Scheduler to Dispatch check_if_reserve_open function at  Intervals
+    :param response: Response of request to the URL
+    :return: None
+    """
+    s = sched.scheduler(time.time, time.sleep)
+    s.enter(INTERVALS, PRIORITY, check_if_reserve_open, (s, response, ))
+    s.run()
 
 
 if __name__ == '__main__':
