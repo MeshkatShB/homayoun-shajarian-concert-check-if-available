a831a4513b9887500dcb9bf734fa81a7cd2d2253
MeshkatShB <meshkat97@yahoo.com>
2022-06-18 10:53:42 +0430

    Fix: no longer stucks at old website

--- main.py
+++ main.py
@@ -1,53 +1,51 @@
 import sched
 import time
 import os
+import requests as req
+
 from bs4 import BeautifulSoup
 from telegram import send
-
-import requests as req
 
 TIWALL_URL = 'https://www.tiwall.com/'
 SALE_URL = 's/'
 PRODUCT_URL = 'p/'
 HOMAYOUN_URL = TIWALL_URL + SALE_URL + 'homayoun.shajarian'
-INTERVALS = 30
+INTERVALS = 3
 PRIORITY = 1
 
+response = req.get(HOMAYOUN_URL, )
 
-def check_if_reserve_open(sc, response):
+
+def check_if_reserve_open(sc):
     """
     Checks if reserve option is available or not
     :param sc: scheduler to dispatch which is activated in each INTERVALS
-    :param response: Response of request to the URL
     :return: None
     """
-
     soup = BeautifulSoup(response.content, 'html.parser')
     div = soup.find_all('select', attrs={'class': 'session', })
+    print(div, len(div))
 
-    if len(div[0]) != 23:
+    if div:
         print('GO GO GO!', )
         os.system('spd-say "You Can Get Your Concert Ticket Now"')
-        # send_telegram_message()
         send()
     else:
+        print('You Should Wait Longer! ROFL!')
         pass
-        # print("No")
 
-    sc.enter(INTERVALS, PRIORITY, check_if_reserve_open, (sc, response))
+    sc.enter(INTERVALS, PRIORITY, check_if_reserve_open, (sc, ))
 
 
-def main(response):
+def main():
     """
     Set Scheduler to Dispatch check_if_reserve_open function at  Intervals
-    :param response: Response of request to the URL
     :return: None
     """
     s = sched.scheduler(time.time, time.sleep)
-    s.enter(INTERVALS, PRIORITY, check_if_reserve_open, (s, response, ))
+    s.enter(INTERVALS, PRIORITY, check_if_reserve_open, (s, ))
     s.run()
 
 
 if __name__ == '__main__':
-    resp = req.get(HOMAYOUN_URL, )
-    main(resp)
+    main()
